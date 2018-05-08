d2dbd476b2053486620117540f1f5a3cea5de369
Sergii Stotskyi <sergiy.stotskiy@gmail.com>
2018-05-08 20:57:33 +0300

    fix(ability): properly check user abilities

--- app/controllers/api/articles_controller.rb
+++ app/controllers/api/articles_controller.rb
@@ -3,37 +3,32 @@ module Api
     load_and_authorize_resource class: "Article"
 
     def index
-      articles = Article.order('id DESC');
+      articles = @articles.order('id DESC')
       render json: { items: articles }
     end
 
     def show
-      article = Article.find(params[:id])
-      render json: { item: article }
+      render json: { item: @article }
     end
 
     def create
-      article = Article.new(article_params)
-
-      if article.save
-        render json: { item: article }, status: 201
+      if @article.save
+        render json: { item: @article }, status: 201
       else
-        render json: { errors: article.errors }, status: :unprocessable_entity
+        render json: { errors: @article.errors }, status: :unprocessable_entity
       end
     end
 
     def destroy
-      article = Article.find(params[:id])
-      article.destroy
+      @article.destroy
       render json: { item: item }, status: :ok
     end
 
     def update
-      article = Article.find(params[:id])
-      if article.update_attributes(article_params)
-        render json: { item: article }
+      if @article.update_attributes(article_params)
+        render json: { item: @article }
       else
-        render json: { errors: article.errors },status: :unprocessable_entity
+        render json: { errors: @article.errors },status: :unprocessable_entity
       end
     end
 
--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -3,37 +3,32 @@ module Api
     load_and_authorize_resource class: "User"
 
     def index
-      users = User.order('id DESC');
+      users = @users.order('id DESC');
       render json: { items: users }
     end
 
     def show
-      user = User.find(params[:id])
-      render json: { item: user }
+      render json: { item: @user }
     end
 
     def create
-      user = User.new(user_params)
-
-      if User.save
-        render json: { item: user }, status: 201
+      if @user.save
+        render json: { item: @user }, status: 201
       else
-        render json: { errors: User.errors }, status: :unprocessable_entity
+        render json: { errors: @user.errors }, status: :unprocessable_entity
       end
     end
 
     def destroy
-      user = User.find(params[:id])
-      User.destroy
-      render json: { item: item }, status: :ok
+      @user.destroy
+      render json: { item: @user }, status: :ok
     end
 
     def update
-      user = User.find(params[:id])
-      if User.update_attributes(user_params)
-        render json: { item: user }
+      if @user.update_attributes(user_params)
+        render json: { item: @user }
       else
-        render json: { errors: User.errors }, status: :unprocessable_entity
+        render json: { errors: @user.errors }, status: :unprocessable_entity
       end
     end
 

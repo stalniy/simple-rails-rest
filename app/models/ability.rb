490f103f73fb206e135554d50164faa5dad5eaf1
Sergii Stotskyi <sergiy.stotskiy@gmail.com>
2018-05-11 15:08:58 +0300

    fix(ability): fixes inverted property in `to_list` method

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -25,7 +25,7 @@ class Ability
     rules.map do |rule|
       object = { actions: rule.actions, subject: rule.subjects.map{ |s| s.is_a?(Symbol) ? s : s.name } }
       object[:conditions] = rule.conditions unless rule.conditions.blank?
-      object[:inverted] = rule.conditions unless rule.base_behavior
+      object[:inverted] = true unless rule.base_behavior
       object
     end
   end

971e716f260814ee8ac345eb16392f30f2d795d9
Sergii <sergiy.stotskiy@gmail.com>
2019-10-28 09:38:15 +0200

    refactor(seeds): replaces create with create! to see errors

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,15 @@
-admin = User.create(email: "admin@freaksidea.com", role: "admin", password:"123456")
-blogger = User.create(email: "member@freaksidea.com", role: "member", password:"123456")
+admin = User.create!(email: "admin@freaksidea.com", role: "admin", password:"123456")
+blogger = User.create!(email: "member@freaksidea.com", role: "member", password:"123456")
 
 2.times do
-  Article.create(
+  Article.create!(
     author_id: admin.id,
     title: Faker::Book.title,
     body: Faker::Lorem.sentence)
 end
 
 5.times do
-  Article.create(
+  Article.create!(
     author_id: blogger.id,
     title: Faker::Book.title,
     body: Faker::Lorem.sentence)

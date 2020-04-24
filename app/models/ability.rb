ddf8d6e4f2f9a5795578fab37acfe129c5b65a60
Sergii Stotskyi <sergiy.stotskiy@gmail.com>
2020-04-24 18:53:13 +0300

    refactor(ability): replaces actions with action
    Fixes #2

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -23,7 +23,7 @@ class Ability
 
   def to_list
     rules.map do |rule|
-      object = { actions: rule.actions, subject: rule.subjects.map{ |s| s.is_a?(Symbol) ? s : s.name } }
+      object = { action: rule.actions, subject: rule.subjects.map{ |s| s.is_a?(Symbol) ? s : s.name } }
       object[:conditions] = rule.conditions unless rule.conditions.blank?
       object[:inverted] = true unless rule.base_behavior
       object
